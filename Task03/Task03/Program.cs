f0f322685271e7126cb8c60c58ecd7a41643480a
Belov-AI <Belov.Alexander@urfu.ru>
2024-09-30 17:34:08 +0500

    Задание выполнено

---
+++ Task03/Task03/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите двузначное число");
+            var number = int.Parse(Console.ReadLine());
+
+            var unints = number % 10;
+            var tens = number / 10;
+            var result = number * 100 + unints * 10 + tens;
+
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
