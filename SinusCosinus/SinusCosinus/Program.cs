1c204f60401888ba4489bb409259159214d89399
Belov-AI <Belov.Alexander@urfu.ru>
2024-10-07 17:12:41 +0500

    Выделен метод

--- SinusCosinus/SinusCosinus/Program.cs
+++ SinusCosinus/SinusCosinus/Program.cs
@@ -10,37 +10,27 @@ namespace SinusCosinus
     {
         static void Main(string[] args)
         {
-            double angleInDegrees = 15;
+            PrintSinusCosinus(15);
+            PrintSinusCosinus(37);
+            PrintSinusCosinus(113);
+
+            Console.WriteLine();
+
+            double angleInDegrees;
+            Console.WriteLine("Введите угол в градусах");
+            angleInDegrees = double.Parse(Console.ReadLine());
+            PrintSinusCosinus(angleInDegrees);
+
+            Console.ReadKey();
+        }
+
+        private static void PrintSinusCosinus(double angleInDegrees)
+        {
             double angleInRadians = angleInDegrees * Math.PI / 180;
             var sin = Math.Sin(angleInRadians);
             var cos = Math.Cos(angleInRadians);
-            Console.WriteLine("sin(15\xB0) = " + Math.Round(sin,3));
-            Console.WriteLine("cos(15\xB0) = " + Math.Round(cos, 3));
-
-            angleInDegrees = 37;
-            angleInRadians = angleInDegrees * Math.PI / 180;
-            sin = Math.Sin(angleInRadians);
-            cos = Math.Cos(angleInRadians);
-            Console.WriteLine("sin(37\xB0) = " + Math.Round(sin, 3));
-            Console.WriteLine("cos(37\xB0) = " + Math.Round(cos, 3));
-
-            angleInDegrees = 113;
-            angleInRadians = angleInDegrees * Math.PI / 180;
-            sin = Math.Sin(angleInRadians);
-            cos = Math.Cos(angleInRadians);
-            Console.WriteLine("sin(113\xB0) = " + Math.Round(sin, 3));
-            Console.WriteLine("cos(113\xB0) = " + Math.Round(cos, 3));
-
-            Console.WriteLine();
-            Console.WriteLine("Введите угол в градусах");
-            angleInDegrees = double.Parse(Console.ReadLine());
-            angleInRadians = angleInDegrees * Math.PI / 180;
-            sin = Math.Sin(angleInRadians);
-            cos = Math.Cos(angleInRadians);
             Console.WriteLine("sin(" + angleInDegrees + "\xB0) = " + Math.Round(sin, 3));
             Console.WriteLine("cos(" + angleInDegrees + "\xB0) = " + Math.Round(cos, 3));
-
-            Console.ReadKey();
         }
     }
 }
