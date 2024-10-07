e5d818012e5e181c8f1ef0a21dd067fbd68e5bad
Belov-AI <Belov.Alexander@urfu.ru>
2024-10-07 17:30:27 +0500

    Выполнено задание 5

---
+++ Task05/Task05/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = F(3, 2) + F(7, 5) + F(2, 7) + F(5, 11);
+
+            Console.WriteLine($"x = {x:F3}");
+
+            Console.ReadKey();
+        }
+
+        static double F(double a, double b)
+        {
+            return Math.Sqrt((a + b + Math.Sqrt(a)) / b);
+        }
+    }
+}
